1cc5ed14d9a28c07697d8c1086fbaaf07565e505
ApplecookieGaming <applecookieinfo@gmail.com>
2017-06-29 07:20:07 +0930

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -1,7 +1,13 @@
 from scene import *
 
 class Player (Node):
-	def setup(self, game, pos):
-		player = SpriteNode(color='white', size=(50, 20))
-		player.position = pos
-		game.add_child(player)
+	def setup(self, game, pos, player_type):
+		self.game = game
+		self.player_type = player_type
+		
+		self.player = SpriteNode(color='white', size=(50, 20))
+		self.player.position = pos
+		self.game.add_child(self.player)
+	
+	def update(self):
+		pass
