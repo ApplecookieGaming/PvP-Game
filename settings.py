68f3952b2abbdbe134ca01df88be18a9925e1b31
ApplecookieGaming <applecookieinfo@gmail.com>
2017-08-09 21:55:54 +0930

    Update settings.py

--- settings.py
+++ settings.py
@@ -7,6 +7,11 @@ D_ALPHA = 0.2
 
 
 ## Player
+SHOW_HITBOX = True
+
+P1_COL = '#0000ff'
+P2_COL = '#ff0000'
+
 MAX_SPEED = 5
 MAX_SPEED_D = sqrt((MAX_SPEED**2)/2)
 
@@ -19,19 +24,27 @@ ROT_TIME = 5
 
 MAX_HEALTH = 100
 MAX_ENERGY = 100
+ENERGY_REGEN = 0.5
 
+
+## Resource Bars
 BAR_DIST = 185
 BAR_HEIGHT = 5
-DEP_TIME = 360 # Depletion rate in miliseconds
 
-HF_COL = '#ff0000'
-HB_COL = '#810000'
+HF_COL = '#ff0000'  # Health bar (fore)
+HB_COL = '#810000'  # Health bar (back)
 
-EF_COL = '#0098ff'
-EB_COL = '#004c80'
+EF_COL = '#0098ff'  # Energy bar (fore)
+EB_COL = '#004c80'  # Energy bar (back)
+
 
 ## Bullet
-BULLET_SPEED = 5
+BULLET_SPEED = 10
 SHOOT_RATE = 10  # miliseconds between shots
 BULLET_COL = '#f3ff00'
-BULLET_DAMG = 10
+BULLET_DAMG = 20
+BULLET_DEP = 10
+
+
+## Effects
+FX_DESPAWN = 200  # In miliseconds
