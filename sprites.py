5075c9fa1f3c933b3c75057379d596de289312bc
ApplecookieGaming <applecookieinfo@gmail.com>
2017-08-09 21:53:22 +0930

    Add better hitboxes
    Add better hitboxes to more 'accurately' determine bullet collision

--- sprites.py
+++ sprites.py
@@ -1,7 +1,7 @@
 from scene import *
 from main import *
 from settings import *
-from math import sin, cos, degrees, radians, sqrt
+from math import sin, cos, tan, degrees, radians, sqrt
 
 
 class Player (Node):
@@ -16,7 +16,9 @@ class Player (Node):
 		self.health = MAX_HEALTH
 		self.energy = MAX_ENERGY
 		self.cooldown = 0
+		self.hitboxes = []
 		
+		## Resource bars
 		if self.player_type == 1:
 			# Health bar
 			self.h_bar1 = ResourceBar()
@@ -37,6 +39,8 @@ class Player (Node):
 			e_bar2_pos = (self.game.size.w / 2, self.game.size.h - BAR_DIST)
 			self.e_bar2.setup(self.game, self, e_bar2_pos, self.player_type, 'energy')
 		
+		self.setup_hitboxes()
+		
 		self.rot = 0
 		self.vel = Vector2(0, 0)
 		self.acc = Vector2(0, 0)
@@ -45,8 +49,13 @@ class Player (Node):
 		self.left_pad()
 		self.right_pad()
 		
+		self.update_hitbox()
+		
 		if self.cooldown > 0:
 			self.cooldown -= 1
+		
+		if self.energy < MAX_ENERGY:
+			self.energy += ENERGY_REGEN
 		
 		# Limits max/min acceleration
 		if self.acc[0] > MAX_ACC:
@@ -66,6 +75,9 @@ class Player (Node):
 		elif self.player_type == 2:
 			self.h_bar2.update()
 			self.e_bar2.update()
+		
+		#if self.health <= 0:
+			#self.kill()
 	
 	# Function to rotate player in the correct direction
 	def rotate(self, deg):
@@ -152,11 +164,14 @@ class Player (Node):
 		right_pad = Main.get_pad_direction(self.game, 'R', self.player_type)
 		
 		if right_pad != 'none':
-			if self.cooldown <= 0:
+			if self.cooldown <= 0 and self.energy >= BULLET_DEP:
 				self.cooldown = SHOOT_RATE
+				
 				bullet = Bullet()
 				bullet.setup(self.game, self, self.player_type)
 				Main.append_bullet_list(self.game, bullet)
+				
+				self.energy -= BULLET_DEP
 			
 			if right_pad == 'up':
 				self.rot = self.rotate(0)
@@ -189,9 +204,13 @@ class Player (Node):
 			if b.get_type() != self.player_type:
 				bullet_rect = b.get_bbox()
 				
-				if self.player.bbox.intersects(bullet_rect):
-					self.health -= BULLET_DAMG
-					Main.kill_bullet(self.game, b)
+				for h in self.player_hitboxes:
+					if h.intersects(bullet_rect):
+						try:							
+							self.health -= BULLET_DAMG
+							Main.kill_bullet(self.game, b)
+						except Exception:
+							pass
 	
 	def get_player(self, player_type):
 		if self.player_type == player_type:
@@ -202,6 +221,87 @@ class Player (Node):
 		
 	def get_energy(self, player_type):
 		return self.energy
+	
+	def kill(self):
+		self.player.remove_from_parent()
+	
+	def setup_hitboxes(self):
+		self.player_hitboxes = []
+		
+		# Middle hitbox
+		mid_x = self.player.position.x
+		mid_y = self.player.position.y
+		mid_w = self.player.size.w / 3
+		mid_h = self.player.size.w / 3
+		self.hitbox_mid = Rect(mid_x, mid_y, mid_w, mid_h)
+		self.player_hitboxes.append(self.hitbox_mid)
+		
+		# Left hitbox
+		left_x = self.player.position.x
+		left_y = self.player.position.y
+		left_w = self.player.size.w / 3
+		left_h = self.player.size.w / 3
+		self.hitbox_left = Rect(left_x, left_y, left_w, left_h)
+		self.player_hitboxes.append(self.hitbox_left)
+		
+		# Right hitbox
+		right_x = self.player.position.x
+		right_y = self.player.position.y
+		right_w = self.player.size.w / 3
+		right_h = self.player.size.w / 3
+		self.hitbox_right = Rect(right_x, right_y, right_w, right_h)
+		self.player_hitboxes.append(self.hitbox_right)
+		
+		if SHOW_HITBOX:
+			# Middle hixbox display
+			self.outline_mid = SpriteNode(texture='assets/outline.png')
+			self.outline_mid.anchor_point = (0, 0)
+			self.outline_mid.position = self.hitbox_mid.origin
+			self.outline_mid.size = self.hitbox_mid.size
+			self.game.add_child(self.outline_mid)
+			
+			# Left hitbox display
+			self.outline_left = SpriteNode(texture='assets/outline.png')
+			self.outline_left.anchor_point = (0, 0)
+			self.outline_left.position = self.hitbox_left.origin
+			self.outline_left.size = self.hitbox_left.size
+			self.game.add_child(self.outline_left)
+			
+			# Right hitbox display
+			self.outline_right = SpriteNode(texture='assets/outline.png')
+			self.outline_right.anchor_point = (0, 0)
+			self.outline_right.position = self.hitbox_right.origin
+			self.outline_right.size = self.hitbox_right.size
+			self.game.add_child(self.outline_right)
+	
+	def update_hitbox(self):
+		# Middle hitbox
+		mid_x = self.player.position.x
+		mid_y = self.player.position.y
+		self.hitbox_mid.center(mid_x, mid_y)
+		
+		# Left hitbox
+		left_x = self.player.position.x - self.hitbox_left.size.w * cos(self.rot)
+		left_y = self.player.position.y - self.hitbox_left.size.h * sin(self.rot)
+		self.hitbox_left.center(left_x, left_y)
+		
+		# Right hitbox
+		right_x = self.player.position.x + self.hitbox_right.size.w * cos(self.rot)
+		right_y = self.player.position.y - self.hitbox_right.size.h * -sin(self.rot)
+		self.hitbox_right.center(right_x, right_y)
+		
+		if SHOW_HITBOX:
+			self.outline_mid.position = self.hitbox_mid.origin
+			self.outline_left.position = self.hitbox_left.origin
+			self.outline_right.position = self.hitbox_right.origin
+		
+		if self.player_type == 1:
+			# print("sin(" + str(self.rot) + ") = " + str(sin(self.rot)) + '	' + "cos(" + str(self.rot) + ") = " + str(cos(self.rot)) + '	' + "tan(" + str(self.rot) + ") = " + str(tan(self.rot)))
+			
+			# print("sin(" + str(self.rot) + ") = " + str(sin(self.rot)))
+			# print("cos(" + str(self.rot) + ") = " + str(cos(self.rot)))
+			# print("tan(" + str(self.rot) + ") = " + str(tan(self.rot)))
+			pass
 	
 	# Code from https://stackoverflow.com/questions/2272149/round-to-5-or-other-number-in-python
 	# By Alok Singhal
@@ -281,7 +381,7 @@ class ResourceBar (Node):
 			elif resource_type == 2:
 				self.back.anchor_point = (0, 1)
 				self.fore.anchor_point = (0, 1)
-		
+				
 		if resource_type == 2:
 			self.back.x_scale = -1
 			self.fore.x_scale = -1
@@ -296,6 +396,7 @@ class ResourceBar (Node):
 		if self.type == 'health':
 			health = Player.get_health(self.player_class, self.resource_type)
 			self.fore.size = (self.back.size.x * (health / MAX_HEALTH), self.back.size.y)
+			
 		elif self.type == 'energy':
 			energy = Player.get_energy(self.player_class, self.resource_type)
 			self.fore.size = (self.back.size.x * (energy / MAX_ENERGY), self.back.size.y)
